014251728bc54c747e670d81ef94caf0fefd2434
Meyiwa Temile <meyiwatemile@gmail.com>
2023-07-27 01:25:09 -0400

    removed svelve-kit

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.e7b628f5.js","_app/immutable/chunks/scheduler.89bf212d.js","_app/immutable/chunks/index.44fa51b5.js","_app/immutable/chunks/singletons.fd1f85e3.js"];
-export const stylesheets = [];
-export const fonts = [];
